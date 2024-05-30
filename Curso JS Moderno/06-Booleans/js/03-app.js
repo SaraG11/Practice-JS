2e850a72310d7157606f8937fd8a46911587d6c0
SG-WORK <gpsara1114@gmail.com>
2024-05-30 10:40:30 -0600

    06-Booleans

---
+++ Curso JS Moderno/06-Booleans/js/03-app.js
@@ -0,0 +1,12 @@
+// mejorando código utilizando boolean
+
+const autenticado = false;
+
+if(autenticado){
+    console.log('Si puedes ver netflix')
+}else{
+    console.log('No puedes ver')
+}
+
+// operador ternario
+console.log( autenticado ? 'Si esta autenticado' : 'No esta autenticado');
