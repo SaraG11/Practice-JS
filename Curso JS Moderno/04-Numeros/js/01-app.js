d6d32b62baa9b6b4518e67a833908b3dfd2abe36
SG-WORK <gpsara1114@gmail.com>
2024-05-29 18:51:37 -0600

    Seccion 3 Numeros 04

---
+++ Curso JS Moderno/04-Numeros/js/01-app.js
@@ -0,0 +1,15 @@
+//Creacion de numeros: todos los numeros se escriben de la misma manera
+
+/*const numero1 = 20;
+const numero2 = '20';
+
+console.log(numero2);*/
+
+//no se tiene que especificar un tipo de dato
+const numero1 = 30;
+const numero2 = 20;
+const numero3 = 20.30;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
---
+++ Curso JS Moderno/04-Numeros/js/03-app.js
@@ -0,0 +1,34 @@
+let resultado;
+
+// PI
+resultado = Math.PI;
+//redondear
+resultado = Math.round(2.5);
+// redondear hacia arriba
+resultado = Math.ceil(2.1);
+
+// redondea hacia abajo 
+resultado = Math.floor(2.9);
+
+// raiz cuadrada
+resultado = Math.sqrt(144);
+
+// absoluto, -200
+resultado = Math.abs(-500);
+
+//potencia
+resultado = Math.pow(2,4);
+
+// minimo
+resultado = Math.min(3, 5, 1 , 13, -3);
+
+// maximo
+resultado = Math.max(3, 5, 1 , 13, -3);
+
+//aleatorio
+resultado = Math.random() * 20;
+
+//aleatorio dentro de un rango 
+resultado = Math.floor(Math.random() * 30);
+
+console.log(resultado);
