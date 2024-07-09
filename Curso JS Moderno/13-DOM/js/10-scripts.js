8036733f4a504d31fdb5a56546b527c9889e74a9
Sara Gonzalez <gpsara1114@gmail.com>
2024-07-08 21:00:36 -0600

    DOM eventos

--- Curso JS Moderno/13-DOM/js/10-scripts.js
+++ Curso JS Moderno/13-DOM/js/10-scripts.js
@@ -34,6 +34,29 @@ parrafo2.classList.add('titulo')
 
 const parrafo3 = document.createElement('p')
 parrafo3.textContent = '800 por persona'
-parrafo3.classList
+parrafo3.classList.add('precio');
 
-console.log(parrafo1)
+// crear div con la clase de info
+const info = document.createElement('div');
+info.classList.add('info');
+info.appendChild(parrafo1)
+info.appendChild(parrafo2)
+info.appendChild(parrafo3)
+
+// crear imagen
+const imagen = document.createElement('img');
+imagen.src = 'img/hacer2.jpg';
+// crear el card
+const card = document.createElement('div')
+card.classList.add('card');
+
+// asignar la imagen
+card.appendChild(imagen)
+
+// asignar info
+card.appendChild(info)
+
+// insertar en el HTML
+const contenedor = document.querySelector('.hacer .contenedor-cards');
+contenedor.appendChild(card);
+contenedor.insertBefore(card, contenedor.children[0])
