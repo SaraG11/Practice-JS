832072cc8e74f11acb26341441a07396d40ec4b6
Sara Gonzalez <gpsara1114@gmail.com>
2024-12-02 10:40:56 -0600

    Spinner

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -12,11 +12,26 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]')
+    const btnReset = document.querySelector('#formulario button[type="reset"]')
+
 
     // Asignar eventos
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
+    formulario.addEventListener('submit', enviarEmail);
+
+    btnReset.addEventListener('click', function(e){
+        e.preventDefault();
+
+        // reiniciar el objeto datos
+        datos.email = '';
+        datos.asunto = '';
+        datos.mensaje = '';
+        formulario.reset();
+        comprobarDatos();
+
+    })
 
     function validar(e){
         if(e.target.value.trim() === ''){
