966140a6252855b8fe3483571a61bc9a2294b3a9
SG-WORK <gpsara1114@gmail.com>
2025-01-07 09:09:21 -0600

    Prototypes

---
+++ Curso JS Moderno/23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -0,0 +1,73 @@
+
+// constructores
+function Seguro(marca, year, tipo){
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+
+function UI(){
+
+}
+// llena las opciones de los años 
+UI.prototype.llenarOpciones = () =>{
+    const max = new Date().getFullYear(),
+          min = max -20;
+    const selectYear = document.querySelector('#year');
+
+    for(let i = max; i > min; i--){
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option)
+    }
+}
+
+// muestra alertas en pantalla 
+UI.prototype.mostrarMensaje = function(mensaje, tipo){
+    const div = document.createElement('div')
+
+    if(tipo === 'error'){
+        div.classList.add('error')
+    }else {
+        div.classList.add('correcto')
+    }
+    div.classList.add('mensaje', 'mt-10')
+    div.textContent = mensaje;
+
+    // insertar en el HTML
+    const formulario = document.querySelector('#cotizar-seguro')
+    formulario.insertBefore(div, document.querySelector('#resultado'))
+
+}
+
+// instanciar UI
+const ui = new UI();
+// console.log(ui)
+
+document.addEventListener('DOMContentLoaded', () =>{
+    ui.llenarOpciones(); //llena el select con los años
+})
+
+eventListeners();
+
+function eventListeners(){
+    const formulario = document.querySelector('#cotizar-seguro')
+    formulario.addEventListener('submit', cotizarSeguro)
+}
+
+function cotizarSeguro(e){
+    e.preventDefault();
+    // leer la marca seleccionada 
+    const marca = document.querySelector('#marca').value
+    // leer año seleccionado
+    const year = document.querySelector('#year').value
+    // leer el tipo 
+    const tipo = document.querySelector('input[name="tipo"]:checked').value
+    
+    if(marca === '' || year === '' || tipo === ''){
+        ui.mostrarMensaje('Todos los campos son obligatorios', 'error')
+    }else{
+        console.log('si paso la validacion')
+    }
+}
